4a1ffc9b8cabe538a0f31f62d216580f09c7bffe
NipunGrover <ngrover5702@conestogac.on.ca>
2023-12-26 01:13:01 -0500

    created lines for the game

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Example Tic Tac Toe board
+        string[][] ticTacToeBoard = new string[][]
+        {
+            new string[] { "", " ", " ", " ", "" },
+            new string[] { "", " ", " ", " ", "" },
+            new string[] { "", " ", " ", " ", "" },
+   
+        };
+
+        // Print the Tic Tac Toe lines
+        PrintTicTacToeLines(ticTacToeBoard);
+    }
+
+    static void PrintTicTacToeLines(string[][] board)
+    {
+        for (int row = 0; row < board.Length; row++)
+        {
+            Console.WriteLine(string.Join(" | ", board[row]));
+
+            if (row < board.Length - 1)
+            {
+                Console.WriteLine(" -------------");
+            }
+        }
+    }
+    /*
+            while (true)
+            { 
+                ConsoleKeyInfo keya = Console.ReadKey(true);
+
+                if (keya.Key == ConsoleKey.DownArrow)
+                {
+                    Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop + 1);
+                }
+                else if (keya.Key == ConsoleKey.UpArrow && Console.CursorTop > 0)
+                {
+                    Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
+                }
+                else if (keya.Key == ConsoleKey.LeftArrow && Console.CursorLeft > 0)
+                { 
+                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                }
+                else if (keya.Key == ConsoleKey.RightArrow)
+                {
+                    Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
+                }
+
+            */
+
+
+
+
+}
