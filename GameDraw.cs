24f5ff8515f05008eca430b679e991792b6913bb
NipunGrover <ngrover5702@conestogac.on.ca>
2023-12-30 15:12:58 -0500

    added new GameDraw class to handle drawing the game

---
+++ GameDraw.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tic_tac_toe
+{
+    internal class GameDraw
+    {
+        static public void PrintTicTacToeLines(string[][] board)
+        {
+            for (int row = 0; row < board.Length; row++)
+            {
+                Console.WriteLine(string.Join(" | ", board[row]));
+
+                if (row < board.Length - 1)
+                {
+                    Console.WriteLine(" -------------");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,57 +1,48 @@
-﻿internal class Program
+﻿namespace tic_tac_toe
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        // Example Tic Tac Toe board
-        string[][] ticTacToeBoard = new string[][]
+        private static void Main(string[] args)
         {
-            new string[] { "", " ", " ", " ", "" },
-            new string[] { "", " ", " ", " ", "" },
-            new string[] { "", " ", " ", " ", "" },
-   
-        };
-
-        // Print the Tic Tac Toe lines
-        PrintTicTacToeLines(ticTacToeBoard);
-    }
-
-    static void PrintTicTacToeLines(string[][] board)
-    {
-        for (int row = 0; row < board.Length; row++)
-        {
-            Console.WriteLine(string.Join(" | ", board[row]));
-
-            if (row < board.Length - 1)
+            // Example Tic Tac Toe board
+            string[][] ticTacToeBoard = new string[][]
             {
-                Console.WriteLine(" -------------");
-            }
+                new string[] { "", " ", " ", " ", "" },
+                new string[] { "", " ", " ", " ", "" },
+                new string[] { "", " ", " ", " ", "" },
+
+            };
+
+            // Print the Tic Tac Toe lines
+            GameDraw.PrintTicTacToeLines(ticTacToeBoard);
         }
-    }
-    /*
-            while (true)
-            { 
-                ConsoleKeyInfo keya = Console.ReadKey(true);
 
-                if (keya.Key == ConsoleKey.DownArrow)
-                {
-                    Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop + 1);
-                }
-                else if (keya.Key == ConsoleKey.UpArrow && Console.CursorTop > 0)
-                {
-                    Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
-                }
-                else if (keya.Key == ConsoleKey.LeftArrow && Console.CursorLeft > 0)
+        /*
+                while (true)
                 { 
-                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-                }
-                else if (keya.Key == ConsoleKey.RightArrow)
-                {
-                    Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
-                }
+                    ConsoleKeyInfo keya = Console.ReadKey(true);
 
-            */
+                    if (keya.Key == ConsoleKey.DownArrow)
+                    {
+                        Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop + 1);
+                    }
+                    else if (keya.Key == ConsoleKey.UpArrow && Console.CursorTop > 0)
+                    {
+                        Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
+                    }
+                    else if (keya.Key == ConsoleKey.LeftArrow && Console.CursorLeft > 0)
+                    { 
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                    }
+                    else if (keya.Key == ConsoleKey.RightArrow)
+                    {
+                        Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
+                    }
+
+                */
 
 
 
 
+    }
 }
