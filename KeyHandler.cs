fdf916e08583c20f6bf789f21821cce53699a683
NipunGrover <ngrover5702@conestogac.on.ca>
2023-12-30 15:37:17 -0500

    added a new file for handling keyboard keys

---
+++ KeyHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tic_tac_toe
+{
+    internal class KeyHandler
+    {
+        static public void KeyboardKeys()
+        {
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.DownArrow)
+                {
+                    Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop + 1);
+                }
+                else if (key.Key == ConsoleKey.UpArrow && Console.CursorTop > 0)
+                {
+                    Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
+                }
+                else if (key.Key == ConsoleKey.LeftArrow && Console.CursorLeft > 0)
+                {
+                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                }
+                else if (key.Key == ConsoleKey.RightArrow)
+                {
+                    Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
+                }
+
+            }
+        }
+}
